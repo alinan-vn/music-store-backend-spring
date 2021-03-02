3df9cfb251d9656b32db6b45b840d2e851dc59f7
Alejandro Linan <jandrolinan@gmail.com>
2021-03-02 09:05:31 -0600

    first commit, no changes

---
+++ src/test/java/org/hcl/musicstore/MusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hcl.musicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
