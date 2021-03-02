2c1b4edb1a62ce5fba3d96d10965c2f15e3a54f2
licoy12 <licoy0012@gmail.com>
2021-03-02 12:13:49 -0600

    added first 7 entities of the model

---
+++ src/main/java/org/hcl/musicstore/model/BillingInfo.java
@@ -0,0 +1,58 @@
+package org.hcl.musicstore.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class BillingInfo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	int id;
+	
+	@Column(name="street_name")
+	String streetName;
+	
+	@Column(name="apartment_number")
+	String apartmentNumber;
+	
+	@Column(name="city")
+	String city;
+	
+	@Column(name="state")
+	String state;
+	
+	@Column(name="credit_card_number")
+	int creditCardNumber;
+	
+	@Column(name="expiration_month")
+	int expirationMonth;
+	
+	@Column(name="expiration_day")
+	int expirationDay;
+	
+	@Column(name="name_on_card")
+	String nameOnCard;
+	
+	@Column(name="pin_number")
+	int pinNumber;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+	private Customer customer;
+	
+}
---
+++ src/main/java/org/hcl/musicstore/model/CartSongItems.java
@@ -0,0 +1,39 @@
+package org.hcl.musicstore.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartSongItems {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	int id;
+	
+	@Column(name="quantity")
+	int quantity;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "song_id", referencedColumnName = "id")
+	private Song song;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shopping_cart_id", nullable=false)
+	private ShoppingCart shoppingCart;
+	
+}
