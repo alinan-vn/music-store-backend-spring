ff4c846c340542857ec63db8f9d3eb9a1ca6233e
licoy12 <licoy0012@gmail.com>
2021-03-03 18:13:58 -0600

    working controller tested with postman

--- src/main/java/org/hcl/musicstore/controller/AlbumController.java
+++ src/main/java/org/hcl/musicstore/controller/AlbumController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,12 @@ public class AlbumController {
 	@Autowired
 	AlbumService albumService;
 
+	@PostMapping("/savealbum")
+	public ResponseEntity<Album> saveAlbum(@RequestBody Album album){
+		albumService.saveAlbum(album);
+		return new ResponseEntity<Album>(HttpStatus.OK);
+	}
+	
 	@GetMapping("/album")
 	public Iterable<Album> getAlbumDetails(){
 
--- src/main/java/org/hcl/musicstore/controller/BillingInfoController.java
+++ src/main/java/org/hcl/musicstore/controller/BillingInfoController.java
@@ -2,12 +2,16 @@ package org.hcl.musicstore.controller;
 
 import java.util.Optional;
 
-import javax.validation.Valid;
 
 import org.hcl.musicstore.model.BillingInfo;
 import org.hcl.musicstore.service.BillingInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class BillingInfoController {
+	private static Logger logger = LoggerFactory.getLogger(BillingInfoController.class);
 	
 	@Autowired
 	BillingInfoService billingInfoService;
 	
 	@PostMapping("/savebillinginfo")
-	public BillingInfo saveBillingInfo(@Valid @RequestBody BillingInfo billingInfo) {
-		return billingInfoService.saveBillingInfo(billingInfo);
+	public ResponseEntity<BillingInfo> saveBillingInfo(@RequestBody BillingInfo billingInfo) {
+		
+		logger.info(billingInfo.toString());
+		billingInfoService.saveBillingInfo(billingInfo);
+		return new ResponseEntity<BillingInfo>(HttpStatus.OK);
+		
 	}
 	
 	@GetMapping("/viewbillinginfo/{id}")
-	public Optional<BillingInfo> showBillingInfo(int id) throws Exception {
-		return billingInfoService.getBillingInfo(id);
-		
+	public ResponseEntity<?> showBillingInfo(@PathVariable Integer id) throws Exception {
+		Optional<BillingInfo> billingInfo = billingInfoService.getBillingInfo(id);
+		return billingInfo.map(response -> ResponseEntity.ok().body(response)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 	
 }
